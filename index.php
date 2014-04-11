b314ff3468d64c5b2a5756dfec7e67dc5cc44f4a
hembone <jasonhemmy@gmail.com>
2014-04-11 11:37:08 -0500

    sqlsvr connect

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<h2>Index.php</h2>
+
 <?php
 
 define("SA_DB_HOST", "72.3.232.199");
@@ -5,14 +7,30 @@ define("SA_DB_NAME", "vtest8Dev");
 define("SA_DB_USER", "CustDev");
 define("SA_DB_PASS", "cdpass1");
 
-$dbconn = mssql_connect(SA_DB_HOST, SA_DB_USER, SA_DB_PASS);
-if($dbconn) {
-	echo 'success';
-	//mssql_select_db(SA_DB_NAME, $dbconn);
-} else {
-	echo 'fail';
+
+$serverName = SA_DB_HOST;
+$connectionInfo = array("Database"=>SA_DB_NAME, "UID"=>SA_DB_USER, "PWD"=>SA_DB_PASS);
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if($conn) {
+	echo 'Connection established.<br />';
+}else{
+	echo 'Connection could not be established.<br />';
+	die('<pre>'.print_r(sqlsrv_errors(), true).'</pre>');
 }
 
+$sql = "spUserGetMaxId";
+$params = array();
 
+$stmt = sqlsrv_query($conn, $sql, $params);
+if($stmt === false) {
+	die( print_r(sqlsrv_errors(), true));
+}
+
+while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+	echo '<pre>'.print_r($row, true).'</pre>';
+}
+
+sqlsrv_free_stmt($stmt);
 
 ?>
