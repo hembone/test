b4c98064283bec52cbb120f1aadabe5214e375d8
hembone <jasonhemmy@gmail.com>
2014-07-29 16:09:14 -0500

    boom

--- index.php
+++ index.php
@@ -9,13 +9,46 @@ define("MONGO_DBPASS", "");
 
 require_once 'mongoHelper_class.php';
 
-$db = new mongoHelper();
-
-$res = $db->geoNear('hours', -87.629798, 41.878114);
+$mongodb = new mongoHelper();
 
 
+$multiplier = 0.000621371192;
+$latitude = 41.878114;
+$longitude = -87.629798;
+$filters = array(
+    'Oil Change'
+);
+$brand = 'kw';
 
 
+if(!empty($filters)) {
+    $service_filters[] = array('brand' => $brand);
+    foreach($filters as $filter) {
+        $service_filters[] = array('services' => array(
+            '$in' => array($filter)
+        ));
+    }
+    $filters_query = array('$and' => $service_filters);
+} else {
+	$filters_query = array('brand' => $brand);
+}
+
+$query = array(
+    'geoNear' => 'dealers'
+    ,'near' => array(
+        'type' => 'Point'
+        ,'coordinates' => array(floatval($longitude), floatval($latitude))
+    )
+    ,'maxDistance' => 321869 // 200 miles converted to meters
+    ,'distanceMultiplier' => $multiplier
+    ,'spherical' => true
+    ,'query' => $filters_query
+    ,'num' => 1000
+);
+$res = $mongodb->command($query);
+
+
+echo '<pre>'.print_r($filters_query, true).'</pre>';
 echo '<pre>'.print_r($res, true).'</pre>';
 
 
--- mongoHelper_class.php
+++ mongoHelper_class.php
@@ -1,75 +1,82 @@
 <?php
 class mongoHelper {
 
-	function __construct() {
-		$dbhost = MONGO_DBHOST;
-		$dbname = MONGO_DBNAME;
-		$dbuser = MONGO_DBUSER;
-		$dbpass = MONGO_DBPASS;
-		//$conn = new MongoClient("mongodb://".$dbhost, array("username"=>$dbuser, "password"=>$dbpass));
-		$conn = new MongoClient("mongodb://".$dbhost);
-		$this->db = $conn->$dbname;
+/*
+	The first parameter in most of these functions is the name of the collection that
+you want to access.  Second one is probaby the query parameters you want to use, this 
+should be in the form of an array/nested array of some sort depending on what you are 
+trying to find/update/insert.
+*/
+    function __construct() {
+        $dbhost = MONGO_DBHOST;
+        $dbname = MONGO_DBNAME;
+        $dbuser = MONGO_DBUSER;
+        $dbpass = MONGO_DBPASS;
+        //$conn = new MongoClient("mongodb://".$dbhost, array("username"=>$dbuser, "password"=>$dbpass));
+        $conn = new MongoClient("mongodb://".$dbhost);
+        $this->db = $conn->$dbname;
+		$this->dbname = $dbname;
+		$this->admin = $conn->admin;
+    }
+
+    private function parse_cursor($cursor) {
+        $count = $cursor->count();
+        $result = [];
+        if($count > 0) {
+            foreach($cursor as $id=>$value) {
+                $result[] = $value;
+            }
+        } else {
+            $result = false;
+        }
+        return $result;
+    }
+
+    public function get($col, $filter=array(), $fields=array()) {
+        $col = $this->db->$col;
+        $cursor = $col->find($filter, $fields);
+        return $this->parse_cursor($cursor);
+    }
+
+    public function get_by_id($col, $id, $fields=array()) {
+        $col = $this->db->$col;
+        $item = $col->findOne(array('_id' => new MongoId($id)), $fields);
+        return $item;
+    }
+
+    public function insert($col, $data=array()) {
+        $col = $this->db->$col;
+        return $col->insert($data);
+    }
+
+    public function update($col, $filter=array(), $update=array()) {
+        $col = $this->db->$col;
+        return $col->update($filter, $update);
+    }
+
+    public function update_by_id($col, $id, $update=array()) {
+        $col = $this->db->$col;
+        return $col->update(array('_id' => new MongoId($id)), $update);
+    }
+
+    public function upsert($col, $filter=array(), $update=array()) {
+        $col = $this->db->$col;
+        return $col->update($filter, $update, array("upsert" => true));
+    }
+
+	public function rename_table($old_name, $new_name) {
+		$query = array('renameCollection'=>"{$this->dbname}.$old_name",'to'=>"{$this->dbname}.$new_name",'dropTarget'=>"true");
+		return $this->admin->command($query);
 	}
 
-	private function parse_cursor($cursor) {
-		$count = $cursor->count();
-		$result = [];
-		if($count > 0) {
-			foreach($cursor as $id=>$value) {
-				$result[] = $value;
-			}
-		} else {
-			$result = false;
-		}
-		return $result;
-	}
-
-	public function get($col, $filter=array(), $fields=array()) {
+	public function create_index($col, $index) {
 		$col = $this->db->$col;
-		$cursor = $col->find($filter, $fields);
-		return $this->parse_cursor($cursor);
+		return $col->createIndex($index);
 	}
 
-	public function getById($col, $id, $fields=array()) {
-		$col = $this->db->$col;
-		$item = $col->findOne(array('_id' => new MongoId($id)), $fields);
-		return $item;
-	}
-
-	public function insert($col, $data=array()) {
-		$col = $this->db->$col;
-		return $col->insert($data);
-	}
-
-	public function update($col, $filter=array(), $update=array()) {
-		$col = $this->db->$col;
-		return $col->update($filter, $update);
-	}
-
-	public function updateById($col, $id, $update=array()) {
-		$col = $this->db->$col;
-		return $col->update(array('_id' => new MongoId($id)), $update);
-	}
-
-	public function upsert($col, $filter=array(), $update=array()) {
-		$col = $this->db->$col;
-		return $col->update($filter, $update, array("upsert" => true));
-	}
-
-	public function geoNear($col, $lat, $lng, $filter=array(), $limit=1000) {
-		$query = array(
-			'geoNear' => $col
-			,'near' => array(
-				'type' => 'Point'
-				,'coordinates' => array($lng, $lat)
-			)
-			,'maxDistance' => 321869
-			,'spherical' => true
-			,'query' => $filter
-			,'num' => $limit
-		);
-		return $this->db->command($query);
-	}
+    public function command($query) {
+        return $this->db->command($query);
+    }
 
 }
 ?>
