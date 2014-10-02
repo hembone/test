c47218e1201539b0fe1c020b83e1ce1f3a74a525
Jason Hemmy <jasonhemmy@gmail.com>
2014-10-02 10:11:06 -0500

    slider updates

--- slider/index.php
+++ slider/index.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <title>slider</title>
 
-<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans">
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Poiret+One|Lobster">
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="main.css">
 
@@ -25,6 +25,10 @@
 		</li>
 		<li>
 			<h1 class="title title2"></h1>
+			<div class="bullet_bg"></div>
+			<div class="bullet bullet1"></div>
+			<div class="bullet bullet2"></div>
+			<div class="bullet bullet3"></div>
 			<div class="slide slide2">
 				<img src="slide2.jpg">
 			</div>
@@ -47,7 +51,11 @@
 	</div>
 	<div>
 		<label>Slide 2 Title</label><br/>
-		<input id="slide2_title" name="slide2_title" value="Built to Last">
+		<input id="slide2_title" name="slide2_title" value="Built to Last"><br/><br/>
+		<label>Bullet List</label><br/>
+		<input id="bullet1_text" name="bullet1_text" value="Bullet point 1"><br/>
+		<input id="bullet2_text" name="bullet2_text" value="Bullet point 2"><br/>
+		<input id="bullet3_text" name="bullet3_text" value="Bullet point 3">
 	</div>
 	<div>
 		<label>Slide 3 Title</label><br/>
@@ -62,8 +70,7 @@ var sequence;
 $(document).ready(function(){
 	var options = {
 		autoPlay: true
-		,autoPlayDelay: 4000
-		//,animateStartingFrameIn: true
+		//,autoPlayDelay: 4000
 	}
 	sequence = $("#sequence").sequence(options).data("sequence");
 
@@ -78,6 +85,18 @@ $(document).ready(function(){
 	});
 	$('#slide3_title').on('keyup blur', function() {
 		$('.title3').html($(this).val());
+	});
+	$('.bullet1').html($('#bullet1_text').val());
+	$('.bullet2').html($('#bullet2_text').val());
+	$('.bullet3').html($('#bullet3_text').val());
+	$('#bullet1_text').on('keyup blur', function() {
+		$('.bullet1').html($(this).val());
+	});
+	$('#bullet2_text').on('keyup blur', function() {
+		$('.bullet2').html($(this).val());
+	});
+	$('#bullet3_text').on('keyup blur', function() {
+		$('.bullet3').html($(this).val());
 	});
 
 });
