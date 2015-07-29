c165dcaac2c73e19dd4ea4f1f2e738677b3d1589
Jason Hemmy <jasonhemmy@gmail.com>
2015-07-29 09:09:09 -0500

    save

--- test/index.php
+++ test/index.php
@@ -3,19 +3,19 @@
 <head>
 <meta charset="utf-8">
 <title>Testing</title>
-<link rel="stylesheet" href="agegate.css">
 <link rel="stylesheet" href="main.css">
 </head>
 <body>
-<div class="main_wrap">
+<div>
 <!--///////////////////////////////////////-->
 
-test
+
+
 
 <!--///////////////////////////////////////-->
 </div>
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="agegate.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/three.js/r71/three.min.js"></script>
 <script src="main.js"></script>
 </body>
 </html>
