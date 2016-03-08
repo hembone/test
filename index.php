8a3c47568eb578325168377d1fc5de69484b1a3d
Jason Hemmy <jasonhemmy@gmail.com>
2016-03-08 11:34:19 -0600

    save

--- index.php
+++ index.php
@@ -3,12 +3,40 @@
 <head>
 <meta charset="UTF-8">
 <title>Test Page</title>
+<script>
+    window.pAsyncInit = function() {
+        PDK.init({
+            appId: "4822219281081058781",
+            cookie: true
+        });
+    };
+
+    (function(d, s, id){
+        var js, pjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//assets.pinterest.com/sdk/sdk.js";
+        pjs.parentNode.insertBefore(js, pjs);
+    }(document, 'script', 'pinterest-jssdk'));
+</script>
 </head>
 <body>
 
 
 
 
-
+<script>
+PDK.request('/v1/me/pins/', function (response) { // Make sure to change the board_id
+    console.log(response);
+    if (!response || response.error) {
+        alert('Error occurred');
+    } else {
+        
+        if (response.hasNext) {
+            response.next(); // this will recursively go to this same callback
+        }
+    }
+});
+</script>
 </body>
 </html>
--- test/index.php
+++ test/index.php
@@ -9,6 +9,56 @@
 <body>
 <div>
 <!--///////////////////////////////////////-->
+<?php
+
+// staging
+// define("RAPP_KEY", "2iDV9QHK9wY2GqOHl9A6AUTb3eIcdTm8");
+// define("RAPP_API_URL", "http://staging.registration.cinq.io:80/api/v1");
+
+// pro
+// define("RAPP_KEY", "EKJ38VzRgxkOTY0wve1EY1K9NVjvRoi3");
+// define("RAPP_API_URL", "http://registration.cinq.io/api/v1");
+//
+// $apiData = array(
+//     'media_id' => 170
+//     ,'status' => 'approved'
+// );
+// $ch = curl_init();
+// curl_setopt($ch, CURLOPT_URL, RAPP_API_URL.'/user/status');
+// curl_setopt($ch, CURLOPT_POST, count($apiData));
+// curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($apiData));
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+// curl_setopt($ch, CURLOPT_HTTPHEADER, array('x-api-key: '.RAPP_KEY));
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+// curl_setopt($ch, CURLOPT_VERBOSE, true);
+// $jsonData = curl_exec($ch);
+// curl_close($ch);
+// $res = json_decode($jsonData);
+//
+// echo '<pre>'.print_r($res,true).'</pre>';
+
+//$jsonData = '{"requestId":"e93aa9c5-4a04-4f93-ad46-abb6e2699af7","responses":[{"recipientSendId":"e93aa9c5-4a04-4f93-ad46-abb6e2699af7","hasErrors":false,"messages":["Queued"]}]}';
+$jsonData = '<html><head><title>502 Bad Gateway</title></head><body bgcolor="white"><center><h1>502 Bad Gateway</h1></center><hr><center>nginx/1.6.2</center></body></html>';
+//$jsonData = '';
+// $res = json_decode($jsonData);
+// echo '<pre>'.print_r($res,true).'</pre>';
+//
+// if(empty($res) || $res->responses[0]->hasErrors) {
+//     echo 'failed';
+//
+//     $to = 'jason.hemmy@fcb.com';
+//     $subject = 'RAPP API Down '.date("Y-m-d H:i:s");
+//     $message = "The RAPP API is down!\r\n";
+//     $message .= "Last Response:\r\n";
+//     $message .= $jsonData."\r\n";
+//     $headers = 'From: MeowMix <admin@irresistiblemoments.com>' . "\r\n";
+//     mail($to, $subject, $message, $headers);
+//
+// }
+
+?>
+
 
 
 <!--///////////////////////////////////////-->
