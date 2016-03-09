60e01e391a0b155f60be0961dce82d9328733a3d
hembone <jasonhemmy@gmail.com>
2016-03-09 12:40:46 -0600

    save

--- index.php
+++ index.php
@@ -22,20 +22,30 @@
 </head>
 <body>
 
-
-
-
+<script src="//assets.pinterest.com/sdk/sdk.js"></script>
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 <script>
-PDK.request('/v1/me/pins/', function (response) { // Make sure to change the board_id
-    console.log(response);
-    if (!response || response.error) {
-        alert('Error occurred');
-    } else {
-        
-        if (response.hasNext) {
-            response.next(); // this will recursively go to this same callback
-        }
+// PDK.request('/v1/me/pins/', function (response) { // Make sure to change the board_id
+//     console.log(response);
+//     if (!response || response.error) {
+//         alert('Error occurred');
+//     } else {
+//
+//         if (response.hasNext) {
+//             response.next(); // this will recursively go to this same callback
+//         }
+//     }
+// });
+var pin = {
+    login : function() {
+        PDK.login({scope: 'read_public'}, pin.loginCallback);
+    },
+    loginCallback : function(res) {
+        console.log(res);
     }
+};
+$(function() {
+    pin.login();
 });
 </script>
 </body>
